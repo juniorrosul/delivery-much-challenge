f5983664e0b02d3158436870b02ea4eebd9b47de
Madson Verdi Junior <junior2590@gmail.com>
2020-09-11 03:19:09 -0300

    Request and Response for internal model.

--- application/recipe/models.go
+++ application/recipe/models.go
@@ -1,19 +1,45 @@
 package recipe
 
-// Model - Recipe model
-type Model struct {
+// Recipe - Internal Recipe model
+type Recipe struct {
 	Title       string `json:"title"`
 	Ingredients string `json:"ingredients"`
 	Link        string `json:"link"`
 	Gif         string `json:"gif"`
 }
 
+// Response - Internal API response
+type Response struct {
+	Keywords []string `json:"keywords"`
+	Recipes  []Recipe `json:"recipes"`
+}
+
+// Request - Internal API request
+type Request struct {
+	Keywords []string `json:"keywords"`
+}
+
 // NewRecipe - Create new Recipe model
-func NewRecipe(title string, ingredients string, link string, gif string) *Model {
-	return &Model{
+func NewRecipe(title string, ingredients string, link string, gif string) *Recipe {
+	return &Recipe{
 		title,
 		ingredients,
 		link,
 		gif,
+	}
+}
+
+// NewResponse - Create new response model
+func NewResponse(keywords []string, recipes []Recipe) *Response {
+	return &Response{
+		keywords,
+		recipes,
+	}
+}
+
+// NewRequest - Create new request model
+func NewRequest(keywords []string) *Request {
+	return &Request{
+		keywords,
 	}
 }
